23d957a13fa0d16663ca4bfb984008627037377a
Nfongeh Queen <queennfongeh@gmail.com>
2022-10-27 16:47:20 -0400

    description of the gym app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>My New Website</h1>
+      <p>
+        THis app is basically going to be able to register members of the gym,
+        give the lessons, advice online and orientate them on how to achieve
+        their desired body size
+      </p>
     </div>
   );
 }
